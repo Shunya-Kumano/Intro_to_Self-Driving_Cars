ebabb9f2864a9d55fad5ee4466c4ca78d1cd18f0
ShunyaKumano <58796747+Shunya-Kumano@users.noreply.github.com>
2019-12-12 14:24:55 +0900

    upload python instruction code

---
+++ Python Variables (Instructions).py
@@ -0,0 +1,9 @@
+# Here we assign a value of 0.5 to the variable named p_heads
+# and then use a print statement.
+p_heads = 0.47558
+print("The probability of heads is", p_heads)
+
+# Here we assign a value to p_tails. This is done by taking the 
+# compli
+p_tails = 1.0 - p_heads
+print("The probability of tails is", p_tails)
